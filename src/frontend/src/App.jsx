55d2c90b23a07b42169d53a77faa6a07d5ba1e42
LeThanhCong <ltchcmus@gmail.com>
2026-01-07 01:35:03 +0700

    fix: add .jsx extension to Toast import for Linux compatibility

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppRoutes from './routes/AppRoutes';
-import { ToastProvider } from './components/common/Toast';
+import { ToastProvider } from './components/common/Toast.jsx';
 import './App.css';
 
 function App() {
